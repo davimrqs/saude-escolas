b17628edbf8fe0999b64da0b71947866c7cf338d
Davi Marques <dmarques60@gmail.com>
2025-03-23 23:40:25 -0300

    Atualizar site

--- app.js
+++ app.js
@@ -5,25 +5,43 @@ const resultDiv = document.getElementById('result');
 let points = 0;
 
 function resetarJogo() {
+    const menu = document.querySelector(".quiz-container");
+    const img = document.getElementById("imagemGanhador");
+    const img2 = document.getElementById("imagemPerdedor");
+    const buttons = document.querySelector(".quiz-container__buttons"); 
+    const radios = document.querySelectorAll('input[type="radio"]');
+    radios.forEach(function(radio) {
+        radio.checked = false;
+    });
     points = 0;
+    buttons.style.display = "none";
+    img.style.display = "none";
+    img2.style.display = "none";
+    menu.classList.remove("hidden");
+    document.getElementById("result").style.display = "none";
 }
 
+// Trocar de código para pegar o ID do quiz e adicionar um listener event para submit
 function showResult() {
-    event.preventDefault();
+
     const q1 = document.querySelector('input[name="q1"]:checked');
     const q2 = document.querySelector('input[name="q2"]:checked');
     const questions = document.querySelectorAll('.question');
+    let formIsValid = true;
     const respostasCorretas = {
         "q1": "b",
         "q2": "a"
     };
-    console.log(q1);
-    console.log(q2);
     questions.forEach(function(question) {
+        
         const selected = question.querySelector('input[type="radio"]:checked')
         if (!selected) {
+            formIsValid = false;
+        if (!formIsValid){
             document.getElementById('result').innerHTML = "Selecione todas as opções antes de enviar o texto.";
             document.getElementById('result').style.color = "red";
+            return;
+        }
         }else{
             const name = selected.name;
             if (selected.value == respostasCorretas[name]){
@@ -36,18 +54,18 @@ function showResult() {
             const buttons = document.querySelector(".quiz-container__buttons");
             buttons.style.display = "flex";
             menu.classList.add("hidden");
+            document.getElementById("result").style.display = "flex";
             if (points == 2){
-                document.getElementById('result').innerHTML = "Você acertou!";
-                document.getElementById('result').style.color = "red";
+                document.getElementById('result').innerHTML = "Você acertou todas as questões. Parabéns!";
+                document.getElementById('result').style.color = "green";
                 img.style.display = "flex";
                 img2.style.display = "none";
             }else if (points > 0 ){
-                document.getElementById('result').innerHTML = "Você acertou com " + points + "tentativas.";
-                document.getElementById('result').style.color = "red";
+                document.getElementById('result').innerHTML = "Você acertou  " + points + " questões de 2. Parabéns!";
+                document.getElementById('result').style.color = "orange";
                 img.style.display = "flex";
                 img2.style.display = "none";
             }else{
-                console.log(points);
                 img.style.display = "none";
                 img2.style.display = "flex";
                 document.getElementById('result').innerHTML = "<strong>Paciência</strong>... tente novamente!";
@@ -79,4 +97,32 @@ menuLinks.forEach(function(link) {
 
 closeMenu.addEventListener('click', () => {
     menu.classList.remove("open");
+});
+
+// Feedback
+var allStars = document.querySelectorAll('.star');
+
+function limparFeedback(event) {
+    event.preventDefault();
+    document.querySelector('textarea[name="resenha"]').value = "";
+
+    allStars.forEach(function(s, i){
+        if (s.classList.contains('ativo')) {
+            s.classList.remove('ativo');
+        }
+        if (i == 0) {
+            s.classList.add('ativo');
+        }
+    });
+
+};
+
+document.addEventListener('click', function(e) {
+    var clickedStar = e.target.classList;
+    if (!clickedStar.contains('ativo')) {
+        allStars.forEach(function(star){
+            star.classList.remove('ativo');
+        });
+        clickedStar.add('ativo');
+    }
 });
